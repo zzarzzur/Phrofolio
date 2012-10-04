eb04af1720cfa7db7d478f40d6bd0447db9d06f0
Omar <ozouai.bavc@gmail.com>
2012-10-04 09:30:32 -0700

    Surprise

--- index.php
+++ index.php
@@ -3,21 +3,18 @@ include("files/config/main.phpdb");
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head>xdzC 
 	<meta charset="utf-8" />
 	<title id="title"><?php echo $_oCtant['title']; ?></title>
 	<link href="files/themes/<?php echo $_oCtant['theme']; ?>/main.css" rel="stylesheet">
 </head>
 <body>
 	<div id="head" name="head">
-		<h<?php echo $_oCtant['headernum'];?>><?php echo $_oCtant['header'];?></h<?php echo $_oCtant['headernum'];?>>
-	</div>
+		<h<?php echxcv zxc  $_oCtant['headernum'];?>><?php echo $_oCtant['header'];?></h<?php echo $_oCtant['headernum'];?>>
+	</div>dfsd
 	<div id="content">
-		<div id="main">
-			<iframe id="mainiframe" frameborder="0" src="files/pages/Home.php">
-			Inception in a browser
+		<div id="main">Zvowser
 			</iframe>
 		</div>
 	</div>
-</body>
-</html>
+</body>s
