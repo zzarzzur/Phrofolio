dc279e7b1c315d1e25835769b3fda758228a7a04
zzarzzur <ozouai.bavc@gmail.com>
2012-10-04 23:21:06 -0700

    Added line breaks inbetween the projects, Created a Project with a brief description of Phrofolio

---
+++ files/projects/this/index.php
@@ -0,0 +1,34 @@
+<?php
+//This is the main index, when a project is selected this page will appear. All you need to have is links to the other pages in this format './pagename.***'
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Project Example</title>
+</head>
+<body>
+This is Phrofolio, a dynamic and innovative new piece of software designed for those who wish for a better portfolio site<br>
+The name "Phrofolio" is derived from a mash-up of "PHP"(the software that runs the code) and Portfolio.<br>
+Future ideas, (note that they are IDEAS, may or may not happen)
+<div>
+<ul>
+	<li>
+		Custom plugin support
+	</li>
+	<li>
+		Builtin Project Uploader
+	</li>
+	<li>
+		Full Documentation(will happen)
+	</li>
+</ul>
+</div>
+<h3>Features</h3>
+<table align="center">
+<tr><td>Customizable Themes</td></tr>
+<tr><td>Project Listing</td></tr>
+<tr><td>All linked in a nice page</td></tr>
+</table>
+</body>
+</html>
--- files/scripts/php/listprojects/index.php
+++ files/scripts/php/listprojects/index.php
@@ -5,7 +5,7 @@ if(is_dir("../projects/"))$dir = glob("../projects/*");
 foreach($dir as $project){
 	$_meta = array();
 	include("" . $project . "/manifest.phpdb");
-	if(is_dir("../../../projects/"))echo "<a href='../viewproject/?projectname=" . substr($project, 18) . "'>" . $_meta['projtitle'] . "</a>";
-	if(is_dir("../projects/"))echo "<a href='../scripts/php/viewproject/?projectname=" . substr($project, 12) . "'>" . $_meta['projtitle'] . "</a>";
+	if(is_dir("../../../projects/"))echo "<a href='../viewproject/?projectname=" . substr($project, 18) . "'>" . $_meta['projtitle'] . "</a><br>";
+	if(is_dir("../projects/"))echo "<a href='../scripts/php/viewproject/?projectname=" . substr($project, 12) . "'>" . $_meta['projtitle'] . "</a><br>";
 
 }
