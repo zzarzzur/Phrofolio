7dbaeba23a914b370313a0bf8ba3da612fe02b0f
zzarzzur <ozouai.bavc@gmail.com>
2012-10-08 13:56:20 -0700

    Working on the local databases

---
+++ files/databases/local/index.php
@@ -0,0 +1,27 @@
+<?php
+include("../../scripts/php/database/main.php");
+$databases = glob("*/");
+foreach($databases as $base) {
+	if(is_dir($base)) {
+	echo substr($base, 0, -1);
+	echo "<table border='1'>";
+	$tables = glob($base . "*");
+	foreach($tables as $table) {
+		echo "<tr>";
+		$dat = explode("\n", file_get_contents($table));
+		foreach($dat as $club) {
+			echo "<td>";
+			echo $club;
+			echo "</td>";
+		}
+		echo "</tr>";
+	}
+	echo "</table>";
+}
+}
+$ar = database_test("users", "Username:Barney;password:troll;role:admin;", 0);
+print_r($ar);
+foreach($ar as $col=>$data){
+	echo $col . "<br>";
+}
+	
--- files/scripts/php/database/main.php
+++ files/scripts/php/database/main.php
@@ -36,5 +36,60 @@ if($base == "int") {
 				if($failedp == true) $er.="passerror;";
 				}
 				}
+			}else {
+$dbtype = "local";
+if($dbtype == "local") {
+        $failedu = true;
+        $failedp = true;
+        $failedr = true;
+if(is_dir("../../../databases"))$fbase = "../../../databases";
+if(is_dir("../databases"))$fbase = "../databases";
+if(is_dir("../local"))$fbase = "..";
+//echo "FBASE:" . $fbase . "<br>";
+        $values = explode("\n", file_get_contents($fbase . "/local/" . $base . "/0"));
+//print_r($values);
+	$dird = array();
+	$i=0;
+	foreach($values as $dirte){ 
+		$dird[$dirte] = array();
+		$datag = glob($fbase . "/local/" . $base . "/*");
+		foreach($datag as $glo) {
+			if(substr($glo, -2) != "/0") {
+				$file = explode("\n", file_get_contents($glo));
+				$dird[$dirte][count($dird[$dirte])] = $file[$i];
 			}
+		}
+		$i++;
+	}
+	return $dird;
+        foreach($data as $dat) {
+                $val = explode(":", $dat);
+                if($val[0] == "username") {
+                        foreach($username as $user) {
+                                if($val[1] == $user)$failedu = false;
+                        }
+                        }
+                if($val[0] == "password") {
+                        foreach($password as $user) {
+                                if($val[1] == $user)$failedp = false;
+                        }
+                        }
+                if($val[0] == "role") {
+                        foreach($role as $user) {
+                                if($val[1] == $user)$failedr = false;
+                        }
+                        }
+                        }
+                        if($failedr == false && $failedu == false && $failedp == false) return true;
+                        else{
+                        if($lev == 0) return false;
+                        if($lev == 1){
+                                $er = "";
+                                if($failedr == true) $er.="roleerror;";
+                                if($failedu == true) $er.="usererror;";
+                                if($failedp == true) $er.="passerror;";
+                                }
+                                }
+                        }
+}
 			}
