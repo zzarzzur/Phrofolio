d8fedbf90a352ad8c6a370e17f859ae4e3e37bc2
zzarzzur <ozouai.bavc@gmail.com>
2012-10-11 23:15:35 -0700

    My Code

--- files/pages/Admin.php
+++ files/pages/Admin.php
@@ -6,24 +6,7 @@ $final = false;
 include("../scripts/php/admin/login.php");
 }else $final = true;
 if($final == true) {
-?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>
-			Admin
-		</title>
-	</head>
-	<body>
-		Welcome to the admin page for Phrofolio.
-		<br>
-		there really isn't anything YET,
-		<br>
-		But I'm working on it so be patient damn it!
-
-	</body>
-</html>
-<?php
+include("Admin/blog.php");
 }
 session_write_close();
 ?>
